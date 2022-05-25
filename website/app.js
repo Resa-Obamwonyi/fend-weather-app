62fc9207f7d5b38c89b9781ac65ea5d59a4b7e5c
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2022-05-25 08:13:34 +0100

    feat-added api credentials

--- website/app.js
+++ website/app.js
@@ -1,10 +1,13 @@
 // Personal API Key for OpenWeatherMap API
+const apiKey = 'c8d5807618fc610b35e6bad125c0c12b&units=imperial';
+const apiBaseUrl = 'api.openweathermap.org';
 
 // Event listener to add function to existing HTML DOM element
 
 /* Function called by event listener */
 
 /* Function to GET Web API Data*/
+const getApiData = async 
 
 /* Function to POST data */
 
