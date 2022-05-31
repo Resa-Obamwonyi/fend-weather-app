4e960b9c6b4e429736706a44450c466db0170baf
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2022-06-01 00:11:32 +0100

    Chained promise for api post

--- website/app.js
+++ website/app.js
@@ -3,6 +3,7 @@ const apiKey = 'c8d5807618fc610b35e6bad125c0c12b&units=imperial';
 const apiBaseUrl = 'https://api.openweathermap.org';
 
 let generateBtn = document.getElementById('generate');
+let feelings = document.getElementById('feelings');
 
 // Event listener to add function to existing HTML DOM element
 
@@ -10,7 +11,14 @@ let generateBtn = document.getElementById('generate');
 generateBtn.addEventListener('click', (e) => {
     e.preventDefault();
     let zipcode = document.getElementById('zip').value;
-    getWeatherData(apiBaseUrl, zipcode, apiKey);
+    getWeatherData(apiBaseUrl, zipcode, apiKey).then((data) => {
+        postProjectData('/post', {
+            'temp': data.main.temp,
+            'date': data.date,
+            'userResponse': feelings
+        })
+    }
+    );
 })
 
 /* Function to GET Web API Data*/
@@ -18,23 +26,32 @@ const getWeatherData = async (baseUrl, zip, key) => {
     const response = await fetch(baseUrl + '/data/2.5/weather?zip=' + zip + '&appid=' + key);
     try {
         const data = await response.json();
-        console.log(data)
+        console.log(data);
+        return data;
     }
-    catch(error){
+    catch (error) {
         console.log('error:', error);
     }
 }
 
 /* Function to POST data */
-const postProjectData = async (urlPath, data) =>{
+const postProjectData = async (urlPath, apiData) => {
     const response = await fetch(url, {
         method: 'POST',
         credentials: 'same-origin',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(apiData),
     });
+
+    try {
+        const newPostData = await response.json();
+        return newPostData;
+    }
+    catch (error) {
+        console.log("error", error);
+    }
 }
 
 /* Function to GET Project Data */
